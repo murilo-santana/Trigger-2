bba163f8204f4225afdb1e72a567e48aa0928a4d
Murilo Santana <murilogoncalves784@gmail.com>
2026-03-23 18:38:52 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -13,8 +13,7 @@ HEADERS = {
 
 # Lista de workflows a serem acionados
 WORKFLOWS = [
-    {"repo": "CPT30min_aviso", "workflow": "aviso_CPT.yaml"},
-    {"repo": "robo_inbound", "workflow": "main_inbound.yaml"},
+    {"repo": "piso_exp", "workflow": "piso10.yaml"},
 ]
 
 # Rota principal para verificar se o app está no ar
